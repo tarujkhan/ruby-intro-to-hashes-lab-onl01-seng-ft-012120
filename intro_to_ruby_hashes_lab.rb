827bf553eaeefaf154ab18dfb4ac9fb51c058ea0
taroj khan <tarujkhan@yahoo.com>
2020-02-02 23:23:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,16 +29,17 @@ def monopoly_with_first_tier
       {
         "mortgage_value"=>"$100"
       },
-}
+  }
+end
 def monopoly_with_second_tier
   monopoly_with_second_tier = {
     :pennsylvania_railroad=>
       {
         "mortgage_value"=>"$200"
       },
-  }
+  
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+}
 end
 
 def monopoly_with_third_tier
