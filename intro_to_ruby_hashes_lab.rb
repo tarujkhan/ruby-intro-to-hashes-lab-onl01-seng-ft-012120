1a4972bbe4134bbd7de010939d660e7658d55449
taroj khan <tarujkhan@yahoo.com>
2020-02-02 23:16:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,12 +9,22 @@ actor = {
 end
 
 def monopoly
-	monopoly = {
+	monopoly = {:railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
 	  
-}
+{
 	
 
-end
+def monopoly_with_first_tier
+  monopoly_with_first_tier = {
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
